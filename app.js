bcb77a220ac5931931faae93e458ee4791078b63
m-trex <kabo2g@gmail.com>
2021-01-24 07:03:19 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const cityform = document.querySelector('form');
+const card = document.querySelector('.card') ;
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time')
+const icon = document.querySelector('.icon img')
+const updateui = (data) =>{
+    // const cityDets = data.cityDets ;
+    // const weather = data.weather ; 
+    const{cityDets , weather}= data;
+
+    details.innerHTML= `
+    
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;c</span>
+    </div>
+    ` ; 
+    const iconsrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src' ,iconsrc);
+   
+    const timesrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg' ;
+  
+    time.setAttribute('src' ,timesrc) ;
+
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none')
+        
+    }
+
+}
+
+
+const updatecity = async(city) => {
+    const cityDets = await getcity(city) ;
+    const weather = await getwe(cityDets.Key);
+    return {
+      cityDets , weather 
+    }
+
+
+};
+cityform.addEventListener('submit' , e => {
+    e.preventDefault();
+    const city = cityform.city.value.trim();
+    cityform.reset();
+    updatecity(city)
+    .then(data => updateui(data))
+    .catch(err => console.log(err));
+
+});
---
+++ caste.js
@@ -0,0 +1,25 @@
+const key = 'prYzaaRvtZiKq7kCQGAtrv3yYZjjrIfs' ;
+
+const getwe = async(id)=> {
+    const base ='https://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+    const response = await fetch(base+query) ;
+    const data = await response.json();
+    return data[0];
+
+};
+
+const getcity = async(city) => {
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search' ;
+    const query = `?apikey=${key}&q=${city}`;
+    const response = await fetch(base + query );
+    const data = await response.json();
+    return data[0];
+} ;
+
+// getcity('cairo')
+// .then (data => {
+//     return getwe(data.Key);
+// }).then(data => {console.log(data);})
+// .catch(err =>console.log(err)
+// );
